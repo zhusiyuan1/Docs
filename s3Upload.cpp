f6f07c98fe6b605d61991145b8bfb05ef6aeaa8e
Zhu Siyuan <41563855+zhusiyuan1@users.noreply.github.com>
2019-10-17 10:24:58 +0800

    Update s3Upload.cpp

--- s3Upload.cpp
+++ s3Upload.cpp
@@ -9,84 +9,73 @@
 #include <aws/core/Aws.h>
 #include <aws/s3/S3Client.h>
 #include <aws/s3/model/PutObjectRequest.h>
+#include <aws/s3/model/GetObjectRequest.h>
+#include <aws/s3/model/DeleteObjectRequest.h>
+#include <aws/s3/model/ListObjectsRequest.h>
+#include <aws/core/auth/AWSCredentialsProvider.h>
+#include <aws/s3/model/Object.h>
+#include <aws/core/http/Scheme.h>
 #include <iostream>
 #include <fstream>
-#include <sys/stat.h>
-//snippet-end:[s3.cpp.put_object.inc]
-
-/**
- * Check if file exists
- * 
- * Note: If using C++17, can use std::filesystem::exists()
- */
-inline bool file_exists(const std::string& name)
-{
-    struct stat buffer;
-    return (stat(name.c_str(), &buffer) == 0);
-}
-
-/**
- * Put an object into an Amazon S3 bucket
- */
-bool put_s3_object(const Aws::String& s3_bucket_name, 
-    const Aws::String& s3_object_name, 
-    const std::string& file_name)
-{
-    // Verify file_name exists
-    if (!file_exists(file_name)) {
-        std::cout << "ERROR: NoSuchFile: The specified file does not exist" 
-            << std::endl;
-        return false;
-    }
-
-    Aws::Client::ClientConfiguration clientConfig;
-    clientConfig.endpointOverride = Aws::String("s3.cn-north-1.jdcloud-oss.com");  // 使用京东云OSS的域名
-    Aws::String ak = "xxx"; 
-    Aws::String sk = "xxx";
-    Aws::S3::S3Client s3_client(Aws::Auth::AWSCredentials(ak,sk),clientConfig);  //使用京东云AKSK、endpoint构造s3client
-    
-    Aws::S3::Model::PutObjectRequest object_request;
-
-    object_request.SetBucket(s3_bucket_name);
-    object_request.SetKey(s3_object_name);
-    const std::shared_ptr<Aws::IOStream> input_data = 
-        Aws::MakeShared<Aws::FStream>("SampleAllocationTag", 
-                                      file_name.c_str(), 
-                                      std::ios_base::in | std::ios_base::binary);
-    object_request.SetBody(input_data);
-
-    // Put the object
-    auto put_object_outcome = s3_client.PutObject(object_request);
-    if (!put_object_outcome.IsSuccess()) {
-        auto error = put_object_outcome.GetError();
-        std::cout << "ERROR: " << error.GetExceptionName() << ": " 
-            << error.GetMessage() << std::endl;
-        return false;
-    }
-    return true;
-    // snippet-end:[s3.cpp.put_object.code]
-}
-
-/**
- * Exercise put_s3_object()
- */
 int main(int argc, char** argv)
 {
-
+    const Aws::String bucket_name = "lewzylu05-1252448703";
+    const Aws::String key_name = "a.cpp";
+    const Aws::String file_name_in = "a.cpp";
+    const Aws::String file_name_out = "s.cpp";
+    
     Aws::SDKOptions options;
     Aws::InitAPI(options);
+    std::cout << "Objects in S3 bucket: " << bucket_name << std::endl;
     {
-        // Assign these values before running the program
-        const Aws::String bucket_name = "BUCKET_NAME";
-        const Aws::String object_name = "OBJECT_NAME";
-        const std::string file_name = "FILE_NAME";
-
-        // Put the file into the S3 bucket
-        if (put_s3_object(bucket_name, object_name, file_name, region)) {
-            std::cout << "Put file " << file_name 
-                << " to S3 bucket " << bucket_name 
-                << " as object " << object_name << std::endl;
+        //config
+        Aws::Client::ClientConfiguration clientConfig;
+        clientConfig.endpointOverride = Aws::String("s3.cn-north-1.jdcloud-oss.com");  // 使用京东云OSS的域名
+        Aws::String ak = "your-ak";
+        Aws::String sk = "your-sk";
+        Aws::S3::S3Client s3_client(Aws::Auth::AWSCredentials(ak,sk),clientConfig);  //使用京东云AKSK、endpoint构造s3client
+        
+        //upload object
+        Aws::S3::Model::PutObjectRequest put_object_request;
+        put_object_request.WithBucket(bucket_name).WithKey(key_name);
+        auto input_data = Aws::MakeShared<Aws::FStream>("PutObjectInputStream",file_name_in.c_str(), std::ios_base::in);
+        put_object_request.SetBody(input_data);
+        auto put_object_outcome = s3_client.PutObject(put_object_request);
+        if (put_object_outcome.IsSuccess()) {
+            std::cout << "Done!" << std::endl;
+        } else {
+            std::cout << "GetObject error: " <<
+                put_object_outcome.GetError().GetExceptionName() << " " <<
+                put_object_outcome.GetError().GetMessage() << std::endl;
+        }
+        
+        //download object
+        Aws::S3::Model::GetObjectRequest get_object_request;
+        get_object_request.WithBucket(bucket_name).WithKey(key_name);
+        auto get_object_outcome = s3_client.GetObject(get_object_request);
+        if (get_object_outcome.IsSuccess()) {
+            Aws::OFStream local_file;
+            local_file.open(file_name_out.c_str(), std::ios::out | std::ios::binary);
+            local_file << get_object_outcome.GetResult().GetBody().rdbuf();
+            std::cout << "Done!" << std::endl;
+        } else {
+            std::cout << "GetObject error: " <<
+                get_object_outcome.GetError().GetExceptionName() << " " <<
+                get_object_outcome.GetError().GetMessage() << std::endl;
+        }
+        
+        //delete object
+        Aws::S3::Model::DeleteObjectRequest del_object_request;
+        del_object_request.WithBucket(bucket_name).WithKey(key_name);
+        auto delete_object_outcome = s3_client.DeleteObject(del_object_request);
+        if (delete_object_outcome.IsSuccess()) {
+            std::cout << "Done!" << std::endl;
+        } else {
+            std::cout << "DeleteObject error: " <<
+                delete_object_outcome.GetError().GetExceptionName() << " " <<
+                delete_object_outcome.GetError().GetMessage() << std::endl;
         }
     }
+
     Aws::ShutdownAPI(options);
 }
